73a64009bfef1bb3d93e4e564fdb5261cd8752a9
lottaquestions <nickothieno@yahoo.com>
2025-04-29 16:34:05 -0400

    Exploring types further

--- Chapter3_Fundamental_Types/types/src/main.rs
+++ Chapter3_Fundamental_Types/types/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 fn build_vector() -> Vec<i16>{
     let mut v = Vec::new();
     v.push(10);
@@ -63,6 +65,88 @@ fn main() {
     // a signed n-bit type can represent -2^n-1 but not 2^n-1
     assert_eq!((-128_i8).checked_div(-1), None);
 
+    // The first product can be represented as a u16;
+    // the second cannot, so we get 250000 modulo 2^16
+    assert_eq!(100_u16.wrapping_mul(200), 20000);
+    assert_eq!(500_u16.wrapping_mul(500), 53392);
 
-    println!("Hello, world!");
+    // Operations on signed types may wrap to negative values.
+    assert_eq!(500_i16.wrapping_mul(500), -12144);
+
+    // In bitwise shift operations, the shift distance
+    // is wrapped to fall within the size of the value.
+    // So a shift of 17 bits in a 16-bit type is a shift of 1
+    assert_eq!(5_i16.wrapping_shl(17), 10);
+
+    // Saturating operations perform clamping in case overflow
+    // or underflow is about to happen
+    assert_eq!(32760_i16.saturating_add(10), 32767);
+    assert_eq!((-32760_i16).saturating_sub(10), -32768);
+
+    // Overflowing operations return a tuple (result, overflowed), 
+    // where result is what the wrapping version of the function 
+    // would return, and overflowed is a bool indicating 
+    // whether an overflow occurred
+    assert_eq!(255_u8.overflowing_sub(2), (253, false));
+    assert_eq!(255_u8.overflowing_add(2), (1, true));
+
+    // A shift of 17 bits is too large for `u16`, and 17 modulo 16 is 1
+    assert_eq!(5_u16.overflowing_shl(17), (10, true));
+
+    // f32 and f64 associated constants
+    assert!((-1. / f32::INFINITY).is_sign_negative());
+    assert_eq!(-f32::MIN, f32::MAX);
+
+    // Floating point number types have the full complement
+    // of methods for mathematical calculations
+    assert_eq!(5f32.sqrt() * 5f32.sqrt(), 5.);
+
+    // method calls have a higher precedence than prefix operators, 
+    // so be sure to correctly parenthesize method calls on 
+    // negated values
+    assert_eq!((-1.01f64).floor(), -2.0);
+
+    // Types can be put on either the literal or the function
+    println!("{}", (2.0_f64).sqrt());
+    println!("{}", f64::sqrt(2.0));
+
+    // Bool type can be converted to an integer in Rust
+    assert_eq!(false as i32, 0);
+    assert_eq!(true as i32, 1);
+
+    // Chars in Rust are 32-bit unicode.
+    // Chars can be converted to integer types, but if the
+    // destination integer type is less than 32 bits, the
+    // upper bits of the char will be truncated
+    assert_eq!('*' as i32, 42);
+    assert_eq!('ಠ' as u16, 0xca0);
+    assert_eq!('ಠ' as i8, -0x60); // U+0CA0 truncated to eight bits, signed
+
+    // The standard library provided some usefule functions for char
+    assert_eq!('*'.is_alphabetic(), false);
+    assert_eq!('β'.is_alphabetic(), true);
+    assert_eq!('8'.to_digit(10), Some(8));
+    assert_eq!('ಠ'.len_utf8(), 3);
+    assert_eq!(std::char::from_digit(8, 10), Some('8'));
+
+    // Tuples
+    // Rust uses tuples to return multiple values from a function
+    // The separate return values can be assigned to individual
+    // variables
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+
+    // The zero-tuple (), also called the unit type is used in cases
+    // where there’s no meaningful value to carry, but context 
+    // requires some sort of type nonetheless
+    // fn swap<T>(x: &mut T, y: &mut T) -> ();
+
+    // Allocating on the heap
+    let t = (12, "eggs");
+    let b = Box::new(t);
+    assert_eq!(*b, t);
+    println!("{}, {}", b.deref().0, b.deref().1);
+
 }
